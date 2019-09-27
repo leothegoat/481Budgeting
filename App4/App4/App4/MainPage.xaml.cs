cd9bb94acb32000367cfd39cedc4956785d94dd7
avina007 <avina007@cougars.csusm.edu>
2019-09-26 18:34:54 -0700

    Fixed file location source

--- App4/App4/App4/MainPage.xaml.cs
+++ App4/App4/App4/MainPage.xaml.cs
@@ -17,5 +17,10 @@ namespace App4
         {
             InitializeComponent();
         }
+
+        void Register_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Register());
+        }
     }
 }
