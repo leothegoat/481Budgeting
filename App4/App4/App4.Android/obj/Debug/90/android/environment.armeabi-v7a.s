334c7c546a5e3fd840cc9ed5697379d90d838b01
leothegoat <54689660+leothegoat@users.noreply.github.com>
2019-11-16 14:01:09 -0800

    Updated Register Page
    No duplicate usernames can be created, no sign in errors

--- App4/App4/App4.Android/obj/Debug/90/android/environment.armeabi-v7a.s
+++ App4/App4/App4.Android/obj/Debug/90/android/environment.armeabi-v7a.s
@@ -82,7 +82,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",%progbits,1
 	.type	.L.str.8, %object
 .L.str.8:
-	.asciz	"459b4dc7-40f8-4236-877d-3715c14e9406"
+	.asciz	"0ef8147a-391f-4ba8-a5e3-58d45ca90ed3"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",%progbits,1
 	.type	.L.str.9, %object
